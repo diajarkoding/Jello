e5a8aba87ec21e696d8a6773407e20a0bd0f4f49
Iskandar Muhaemin <iskandar.muhaemin@danasaham.co.id>
2025-06-18 16:42:23 +0700

    ProductScreen add Header and ItemProduct

--- feature/home/src/main/java/com/iskan/home/ui/product/ProductScreen.kt
+++ feature/home/src/main/java/com/iskan/home/ui/product/ProductScreen.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -19,8 +25,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.iskan.home.ui.ScreenContent
 import com.iskan.ui.components.JelloImageViewClick
+import com.iskan.ui.components.JelloImageViewPhotoUrlRounded
 import com.iskan.ui.components.JelloTextRegular
+import com.iskan.ui.components.RatingBar
 import com.iskan.ui.theme.Gray
+import com.iskan.ui.theme.LightGrayishBlue
+import com.iskan.ui.theme.VeryLightGray
+import com.iskan.ui.theme.VividRed
 
 @Composable
 fun ProductScreen(){
@@ -61,6 +72,36 @@ fun ProductScreen(){
             }
         }
 
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth(),
+            thickness = 2.dp,
+            color = VeryLightGray
+
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            JelloTextRegular(
+                text = "NEW PRODUCT",
+                modifier = Modifier.weight(1f)
+            )
+            JelloImageViewClick(
+                imageVector = Icons.Default.Search,
+                color = LightGrayishBlue
+            )
+            JelloImageViewClick(
+                imageVector = Icons.Default.Menu,
+                color = LightGrayishBlue
+            )
+        }
+
+        ItemProduct()
+
     }
 }
 
@@ -68,4 +109,50 @@ fun ProductScreen(){
 @Composable
 fun ProductScreenPreview(){
     ProductScreen()
+}
+
+@Composable
+fun ItemProduct() {
+    LazyColumn(
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .padding(horizontal = 16.dp)
+            .background(Color.White)
+    ) {
+        items(10) {
+            Row {
+                Card(
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                        .size(100.dp)
+                        .clickable { /* Handle click */ },
+                    colors = CardDefaults.cardColors(
+                        containerColor = LightGrayishBlue
+                    )
+                ) {
+                    JelloImageViewPhotoUrlRounded(
+                        url = "https://picsum.photos/200/300",
+                        description = ""
+                    )
+                }
+                Spacer(modifier = Modifier.padding(start = 8.dp))
+                Column {
+                    JelloTextRegular(
+                        text = "Product Name",
+                        modifier = Modifier,
+                    )
+                    JelloTextRegular(
+                        text = "Rp. 100.000",
+                        modifier = Modifier.padding(top = 7.dp),
+                        color = VividRed
+                    )
+                    RatingBar(
+                        rating = 2f,
+                        modifier = Modifier.padding(top = 18.dp),
+                        onRatingChanged = { /* Handle rating change */ }
+                    )
+                }
+            }
+        }
+    }
 }
